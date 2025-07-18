45f9d52a12671d65806ad065f1689a90a26a760f
Ooxye29 <ricoerfan1@gmail.com>
2025-07-18 17:13:13 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function goToForm() {
+  document.getElementById("video-container").style.display = "none";
+  document.getElementById("form-phase").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("userForm");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const name = document.getElementById("name").value;
+      const dob = new Date(document.getElementById("dob").value);
+      localStorage.setItem("username", name);
+      localStorage.setItem("dob", dob.toISOString());
+      window.location.href = "minigame.html";
+    });
+  }
+});
